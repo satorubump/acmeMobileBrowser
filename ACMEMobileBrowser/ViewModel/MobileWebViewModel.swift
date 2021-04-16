ad4feeafd72917f550531f9ddf9f5b8164e18e79
Satoru <satoru@b-bump.net>
2021-04-15 17:24:08 -0700

    add history saving

--- ACMEMobileBrowser/ViewModel/MobileWebViewModel.swift
+++ ACMEMobileBrowser/ViewModel/MobileWebViewModel.swift
@@ -14,6 +14,18 @@ class MobileWebViewModel : ObservableObject {
     @Published var updateFlag = false
     @Published var history = [History]()
     
+    init() {
+        print("viewModel init")
+        guard let historyUrls = UserDefaults.standard.array(forKey: "historyUrls") as? [String] else { print("urls"); return }
+        guard let historyImages = UserDefaults.standard.array(forKey: "historyImages") as? [Data] else { print("images"); return }
+        for i in 0..<historyUrls.count {
+            let image = UIImage(data: historyImages[i])
+            let history = History(url: historyUrls[i], shot: image!)
+            self.history.append(history)
+        }
+        print("viewModel init 2")
+    }
+    
     func hasBookmark(index: Int) -> Bool {
         guard let bookmarks = UserDefaults.standard.array(forKey: "bookmarks") else {   return false }
         let burls = bookmarks as! [String]
@@ -66,7 +78,22 @@ class MobileWebViewModel : ObservableObject {
             let hist = History(url: url, shot: shot)
             self.history.insert(hist, at: 0)
         }
+        self.saveHistory()
         print("updateHistory 2 \(self.history.count)")
+    }
+    
+    private func saveHistory() {
+        print("saveHistory 1")
+        var urls = [String]()
+        var images = [Data]()
+        self.history.forEach { hist in
+            urls.append(hist.url)
+            images.append(hist.shot.jpegData(compressionQuality: 1)!)
+            print(images)
+        }
+        UserDefaults.standard.set(urls, forKey: "historyUrls")
+        UserDefaults.standard.set(images, forKey: "historyImages")
+        print("saveHistory 2")
     }
 }
 
